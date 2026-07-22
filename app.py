ad4891b0a05c704754e0dd411805291429aa110d
KDarbha19 <Kausthubhd@gmail.com>
2026-07-22 10:49:48 -0400

    changes to fit updated langchain library

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-import os 
+import os
 import json
+import tempfile
 from flask import Flask, request, jsonify, render_template, session
-from langchain_community.document_loaders import pyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_ollama import OllamaLLM
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-import tempfile 
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 app = Flask(__name__)
-app.secret_key = 'pdfchat_secret_key'
+app.secret_key = "pdfchat_secret_key"
 
-#Model Setup
 print("Loading embedding model...")
-#Embeddings convert text to vectors
 embeddings = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-l6-v2",
-    model_kwargs = {"device": "mps"}
+    model_name="all-MiniLM-L6-v2",
+    model_kwargs={"device": "mps"}
 )
 
 print("Connecting to Ollama...")
 llm = OllamaLLM(model="llama3.2:3b")
-print("Model setup complete.")
+print("Ready")
+
+vector_stores = {}
+chat_histories = {}
